3f67c58a84a4e3d2daab5caf979613ca5d68d9e2
DuroGFX <davidwright13503@gmail.com>
2021-11-05 18:31:45 +0000

    Canvas API Scanner

--- Canvas Courses/main.py
+++ Canvas Courses/main.py
@@ -1,7 +1,14 @@
-from requests import get
+import requests
+from termcolor import colored as c
+from prettytable import PrettyTable
 
-url = "https://canvas.instructure.com/api/v1/courses"
+url = "https://clevelandschools.instructure.com/api/v1/courses"
 headers = {"Authorization": "Bearer 11631~M36BGoMGtgtRoxrjThHE8OlWGaCaUiZPjWAxdimHp7x9Pl7eh8OZJmAN3bBgB78f"}
 
-r = requests.get(url, headers=headers)
-print(r)
+r = requests.get(url, headers=headers).json()
+t = PrettyTable(['Name', 'ID'])
+t.align['Name'] = "l"
+t.align['ID'] = "l"
+
+for k in r: t.add_row([k.get("name","\033[91mNot Available\033[0m"), k["id"]])
+print(t)
