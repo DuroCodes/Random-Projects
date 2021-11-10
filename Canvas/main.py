340f2a532da8052ac5dedb526f3a7c43d1356ff3
DuroGFX <davidwright13503@gmail.com>
2021-11-10 19:41:29 +0000

    fix again

--- Canvas/main.py
+++ Canvas/main.py
@@ -15,7 +15,7 @@ try:
   canvas_link = f"https://{school_code}.instructure.com/api/v1/courses"
   check_date = datetime(day=datetime.now().day-5, month=datetime.now().month, year=datetime.now().year)
 
-  class c:
+  class s:
     bl = '\033[30m'
     r = '\033[31m'
     g = '\033[32m'
@@ -28,73 +28,73 @@ try:
     re = '\033[0m'
 
   def CheckCourses():
-    print(c.re)
+    print(s.re)
     url = f"{canvas_link}?per_page=100"
-    r = requestc.get(url, headers=headers).json()
+    r = requests.get(url, headers=headers).json()
 
-    t = PrettyTable([f'{c.g}Name{c.re}', f'{c.y}ID{c.re}'])
-    t.align[f'{c.g}Name{c.re}'] = "l"
-    t.align[f'{c.y}ID{c.re}'] = "l"
-    t.sortby = f'{c.y}ID{c.re}'
+    t = PrettyTable([f'{s.g}Name{s.re}', f'{s.y}ID{s.re}'])
+    t.align[f'{s.g}Name{s.re}'] = "l"
+    t.align[f'{s.y}ID{s.re}'] = "l"
+    t.sortby = f'{s.y}ID{s.re}'
 
-    for k in r: t.add_row([f'{c.g}{k.get("name",f"{c.r}Not Available")}{c.re}', f'{c.y}{k["id"]}{c.re}']) 
+    for k in r: t.add_row([f'{s.g}{k.get("name",f"{s.r}Not Available")}{s.re}', f'{s.y}{k["id"]}{s.re}']) 
     print(t)
 
   def CheckAssignments(class_id):
-    print(c.re)
+    print(s.re)
     url = f"{canvas_link}/{str(class_id)}/assignments?per_page=100"
-    r = requestc.get(url, headers=headers).json()
+    r = requests.get(url, headers=headers).json()
 
-    t = PrettyTable([f'{c.g}Name{c.re}', f'{c.y}ID{c.re}', f'{c.b}Due Date{c.re}'])
-    t.align[f'{c.g}Name{c.re}'] = "l"
-    t.align[f'{c.y}ID{c.re}'] = "l"
-    t.align[f'{c.b}Due Date{c.re}'] = "l"
-    t.sortby = f'{c.b}Due Date{c.re}'
+    t = PrettyTable([f'{s.g}Name{s.re}', f'{s.y}ID{s.re}', f'{s.b}Due Date{s.re}'])
+    t.align[f'{s.g}Name{s.re}'] = "l"
+    t.align[f'{s.y}ID{s.re}'] = "l"
+    t.align[f'{s.b}Due Date{s.re}'] = "l"
+    t.sortby = f'{s.b}Due Date{s.re}'
 
     for k in r: 
-      due_time = k.get("due_at",f"{c.r}1969-01-01")
+      due_time = k.get("due_at",f"{s.r}1969-01-01")
       if due_time is None: due_time = f"1969-01-01"
       due_date = datetime.strptime(due_time[:10], "%Y-%m-%d")
-      if due_date >= check_date: t.add_row([f'{c.g}{k["name"]}{c.re}', f'{c.y}{k["id"]}{c.re}', f'{c.b}{k.get("due_at",f"{c.r}N/A")[:10]}{c.re}'+c.re])
+      if due_date >= check_date: t.add_row([f'{s.g}{k["name"]}{s.re}', f'{s.y}{k["id"]}{s.re}', f'{s.b}{k.get("due_at",f"{s.r}N/A")[:10]}{s.re}'+s.re])
     print(t)
   def CheckModules(class_id):
-    print(c.re)
+    print(s.re)
     url = f"{canvas_link}/{str(class_id)}/modules"
-    r = requestc.get(url, headers=headers).json()
+    r = requests.get(url, headers=headers).json()
 
-    t = PrettyTable([f'{c.g}Name{c.re}', f'{c.y}ID{c.re}', f'{c.b}Items Count{c.re}'])
-    t.align[f"{c.g}Name{c.re}"] = "l"
-    t.align[f"{c.y}ID{c.re}"] = "l"
-    t.align[f"{c.b}Items Count{c.re}"] = "l"
-    t.sortby = f"{c.y}ID{c.re}"
+    t = PrettyTable([f'{s.g}Name{s.re}', f'{s.y}ID{s.re}', f'{s.b}Items Count{s.re}'])
+    t.align[f"{s.g}Name{s.re}"] = "l"
+    t.align[f"{s.y}ID{s.re}"] = "l"
+    t.align[f"{s.b}Items Count{s.re}"] = "l"
+    t.sortby = f"{s.y}ID{s.re}"
 
-    for k in r: t.add_row([f'{c.g}{k["name"]}{c.re}', f'{c.y}{k["id"]}{c.re}', f'{c.b}{k["items_count"]}{c.re}'])
+    for k in r: t.add_row([f'{s.g}{k["name"]}{s.re}', f'{s.y}{k["id"]}{s.re}', f'{s.b}{k["items_count"]}{s.re}'])
     print(t)
 
   def CheckModuleItems(class_id, module_id):
-    print(c.re)
+    print(s.re)
     url = f"{canvas_link}/{str(class_id)}/modules/{str(module_id)}/items"
-    r = requestc.get(url, headers=headers).json()
+    r = requests.get(url, headers=headers).json()
 
-    module = requestc.get(f"{canvas_link}/{str(class_id)}/modules/{str(module_id)}", headers=headers).json()
+    module = requests.get(f"{canvas_link}/{str(class_id)}/modules/{str(module_id)}", headers=headers).json()
 
-    t = PrettyTable([f"{c.g}Title{c.re}", f"{c.m}Type{c.re}", f"{c.y}ID{c.re}"])
-    t.title = f'{c.b}{module["name"]}{c.re}'
-    t.align[f"{c.g}Title{c.re}"] = "l"
-    t.align[f"{c.m}Type{c.re}"] = "l"
-    t.align[f"{c.y}ID{c.re}"] = "l"
-    t.sortby = f"{c.y}ID{c.re}"
+    t = PrettyTable([f"{s.g}Title{s.re}", f"{s.m}Type{s.re}", f"{s.y}ID{s.re}"])
+    t.title = f'{s.b}{module["name"]}{s.re}'
+    t.align[f"{s.g}Title{s.re}"] = "l"
+    t.align[f"{s.m}Type{s.re}"] = "l"
+    t.align[f"{s.y}ID{s.re}"] = "l"
+    t.sortby = f"{s.y}ID{s.re}"
 
-    for k in r: t.add_row([f'{c.g}{k["title"]}{c.re}', f'{c.m}{k["type"]}{c.re}', f'{c.y}{k["id"]}{c.re}'])
+    for k in r: t.add_row([f'{s.g}{k["title"]}{s.re}', f'{s.m}{k["type"]}{s.re}', f'{s.y}{k["id"]}{s.re}'])
     print(t)
 
   while True:
     system("clear")
-    user_input = input(f'{c.m}1.{c.re} Get Courses\n{c.m}2.{c.re} Get Assignments\n{c.m}3.{c.re} Get Modules\n{c.m}4.{c.re} Get Module Data\nInput mode: {c.m}')
+    user_input = input(f'{s.m}1.{s.re} Get Courses\n{s.m}2.{s.re} Get Assignments\n{s.m}3.{s.re} Get Modules\n{s.m}4.{s.re} Get Module Data\nInput mode: {s.m}')
     if user_input.lower() in ('1', '2', '3', '4'):
       if user_input.lower() == '1': CheckCourses()
-      if user_input.lower() == '2': CheckAssignments(input(f"{c.re}Input Course ID: {c.m}"))
-      if user_input.lower() == '3': CheckModules(input(f"{c.re}Input Course ID: {c.m}"))
-      if user_input.lower() == '4': CheckModuleItems(input(f"{c.re}Input Course ID: {c.m}"), input(f"{c.re}Input Module ID: {c.m}"))
+      if user_input.lower() == '2': CheckAssignments(input(f"{s.re}Input Course ID: {s.m}"))
+      if user_input.lower() == '3': CheckModules(input(f"{s.re}Input Course ID: {s.m}"))
+      if user_input.lower() == '4': CheckModuleItems(input(f"{s.re}Input Course ID: {s.m}"), input(f"{s.re}Input Module ID: {s.m}"))
       break
 except: pass
