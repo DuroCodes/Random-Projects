d99e66d9dc9e28a760cafc365edeeec17016a535
DuroGFX <davidwright13503@gmail.com>
2021-11-10 19:41:12 +0000

    fix colors

---
+++ IP Locator/main.py
@@ -0,0 +1,18 @@
+import json, requests
+from os import system
+from datetime import datetime
+from prettytable import PrettyTable
+from pprint import pprint
+try:
+  class c:
+  bl = '\033[30m'
+  r = '\033[31m'
+  g = '\033[32m'
+  y = '\033[33m'
+  b = '\033[34m'
+  m = '\033[35m'
+  c = '\033[36m'
+  w = '\033[37m'
+  u = '\033[4m'
+  re = '\033[0m'
+except: pass
